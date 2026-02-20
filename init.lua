2865d2b36977ee99439bb6e3837ef1eaaf832625
Patrik Sandenvik <patrik.sandenvik@volvocars.com>
2026-02-20 09:10:34 +0100

    Add keybindings and options
    * Add keybindings and options to the config directory
    * Comment out keybindings using unsupported vim.cmd

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
--- Encoding
-vim.opt.encoding = "utf-8"
-vim.opt.fileencoding = "utf-8"
-
--- Fix Nerd Font icons in visual mode
-vim.opt.ambiwidth = "double"
-
--- Better defaults
-vim.opt.number = true
-vim.opt.mouse = "a"
+require("config.options")
+require("config.keybinds")
